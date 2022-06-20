f790f0c2632c04fd1beef837622b634d645ca882
Darren Huang <darrenhuang1211@gmail.com>
2022-06-20 20:46:32 +0800

    Added expense chart feature

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -3,6 +3,7 @@ import {useState} from 'react';
 import ExpensesList from './ExpensesList';
 import Card from '../UI/Card';
 import ExpensesFilter from './ExpensesFilter';
+import ExpensesChart from './ExpensesChart';
 import './Expenses.css';
 
 function Expenses(props) {
@@ -17,6 +18,7 @@ function Expenses(props) {
     return (
         <Card className='expenses'>
             <ExpensesFilter selected={filteredYear} onYearFilterChange={yearFilterHandler}/>
+            <ExpensesChart expenses={filteredExpenses}/>
             <ExpensesList items={filteredExpenses}/>
         </Card>
     );
